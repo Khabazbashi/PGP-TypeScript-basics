40a1949d524f7cc83d32183686dc4f198372b8c5
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-20 13:12:30 +0200

    Create user state of type UserData

--- react-basics/src/App.tsx
+++ react-basics/src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserData } from "./types";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState<UserData>({
+    name: "",
+    age: 0,
+    address: "",
+  });
+
   return (
     <div className="App">
       <p>Test</p>
@@ -10,3 +17,8 @@ function App() {
 }
 
 export default App;
+
+// Fetch user from `https://randomuser.me/api/`
+// Display at least the users `address`, `age` and `name`.
+// Make it possible to change the state of the name field through form input
+// For each new load of the application, a new user should appear.
